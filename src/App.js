d589634c7ea56f3320ce3b34b5e46e702d9d66d7
Josh Cartagena <jc38@princeton.edu>
2020-02-28 13:08:54 -0500

    removing old markup

--- src/App.js
+++ src/App.js
@@ -22,36 +22,8 @@ class App extends React.Component {
             </ul>
           </navbar>
         </center>
-        {/* <h2>Monday</h2>
-        <Monday events={this.state.events} />
-        <h2>Tuesday</h2>
-        <Tuesday events={this.state.events} />
-        <h2>Wednesday</h2>
-        <Wednesday events={this.state.events} />
-        <h2>Thursday</h2>
-        <Thursday events={this.state.events} />
-        <h2>Friday</h2>
-        <Friday events={this.state.events} />
-        <h2>Saturday</h2>
-        <Saturday events={this.state.events} /> */}
       </div>
     );
   }
-
-  // state = {
-  //   events: []
-  // };
-
-  // componentDidMount() {
-  //   fetch("https://engineering.princeton.edu/newsletter/this-week")
-  //     .then(res => {
-  //       console.log(res);
-  //       return res.json();
-  //     })
-  //     .then(events => {
-  //       this.setState({ events: events });
-  //     })
-  //     .catch(console.log);
-  // }
 }
 export default App;
